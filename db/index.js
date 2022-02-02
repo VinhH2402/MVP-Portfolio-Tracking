4e5e194a59f963a73b298c38a72f279966f88158
VinhH2402 <78180414+VinhH2402@users.noreply.github.com>
2022-02-01 18:49:14 -0800

    complete add account function

--- db/index.js
+++ db/index.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-const { useCallback } = require('react');
-const { Spot } = require('@binance/connector');
-const CoinbasePro = require('coinbase-pro');
+const ccxt = require('ccxt')
 
 
 mongoose.connect('mongodb://localhost:27017/allexchanges', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -34,61 +32,31 @@ const addAccount = (data, callback) => {
       if (result) {
         callback('this account was added')
       } else {
-        if (data.exchange === 'Binance') {
-          //do something with binance keys
-          const burl = 'https://testnet.binance.vision';
-          const client = new Spot(data.API_KEY, data.SECRET_KEY, { baseURL: burl });
-          client.account()
-            .then(() => saveKey(data))
-            .catch(() => callback('keys are invalid'))
+        const { API_KEY, SECRET_KEY, passphrase, exchange } = data;
+        let exchangeName = exchange.toLowerCase().replace('.', '')
+        const config = {
+          apiKey: API_KEY,
+          secret: SECRET_KEY,
+          password: passphrase
+        };
+        //will remove later
+        if(exchangeName === 'coinbase') {
+          exchangeName += 'pro'
         }
 
-        if (data.exchange === 'Coinbase') {
-          console.log('do something with coinbase keys')
-          console.log(data)
-          const key = '8966d631fcb3830a0ac4eb4fed7b754d';
-          const secret = 'BpJbqAVqlg7YGw0uRHr7i071cd3ORNlB+Ola4240ekCiSpDbzlUSY1qRuX3Ff3a6m7amoSZDRQIo2in3uSeftQ==';
-          const passphrase = '1u0budpiwt6';
-          const sandboxURI = 'https://api-public.sandbox.pro.coinbase.com';
-
-          const authedClient = new CoinbasePro.AuthenticatedClient(
-            data.API_KEY,
-            data.SECRET_KEY,
-            data.passphrase,
-            sandboxURI
-          );
-          authedClient.getAccounts()
-            .then(() => saveKey(data))
-            .catch(() => callback('keys are invalid'))
+        const exchangeApi = new ccxt[exchangeName](config);
+        
+        //will edit later
+        if(exchangeName === 'binance' || exchangeName === 'coinbasepro') {
+          exchangeApi.setSandboxMode (true);
         }
 
-        if (data.exchange === 'BinanceUS') {
-          console.log('do something with binanceUS key')
-          const burl = 'https://api.binance.us';
-          const client = new Spot(data.API_KEY, data.SECRET_KEY, { baseURL: burl });
-          client.account()
-            .then(() => saveKey(data))
-            .catch(() => callback('keys are invalid'))
-        }
-
-        if (data.exchange === 'Kucoin') {
-          console.log('do something with Kucoin keys')
-          callback()
-        }
-
-        if (data.exchange === 'Gate.io') {
-          console.log('do something with Gate.io keys')
-          callback()
-        }
+        exchangeApi.fetchBalance()
+          .then(() => saveKey(data))
+          .catch(() => callback('keys are invalid'))
       }
     })
 }
-
-const remove = (exchangeName, callback) => {
-
-}
-
-
 
 module.exports.Keys = Keys;
 module.exports.addAccount = addAccount;
